3793c41918c13df9499956c5e7c328decf3f189b
Yiseul Park <omjl5123@catholic.ac.kr>
2023-07-15 17:25:31 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/keepit/KeepItApplication.java
@@ -0,0 +1,13 @@
+package com.keepit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepItApplication.class, args);
+    }
+
+}
