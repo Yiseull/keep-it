47af95408d3ce20e6905b8bbb65065d3a164a6c5
Yiseul Park <omjl5123@catholic.ac.kr>
2023-07-16 21:11:59 +0900

    feat: 제품 등록 기능 추가

---
+++ src/main/java/com/keepit/product/application/ProductService.java
@@ -0,0 +1,32 @@
+package com.keepit.product.application;
+
+import com.keepit.product.domain.Product;
+import com.keepit.product.dto.ProductCreateRequest;
+import com.keepit.product.dto.ProductResponse;
+import com.keepit.product.infrastructure.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse save(ProductCreateRequest request) {
+        Product product = toEntity(request);
+        Product savedProduct = productRepository.save(product);
+        return new ProductResponse(savedProduct);
+    }
+
+    private static Product toEntity(ProductCreateRequest request) {
+        return Product.builder()
+                .name(request.name())
+                .category(request.category())
+                .startDate(request.startDate())
+                .expirationDate(request.expirationDate())
+                .build();
+    }
+}
---
+++ src/main/java/com/keepit/product/dto/ProductResponse.java
@@ -0,0 +1,25 @@
+package com.keepit.product.dto;
+
+import com.keepit.product.domain.Category;
+import com.keepit.product.domain.Product;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ProductResponse {
+
+    private final long id;
+    private final String name;
+    private final Category category;
+    private final LocalDateTime startDate;
+    private final LocalDateTime expirationDate;
+
+    public ProductResponse(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.category = product.getCategory();
+        this.startDate = product.getStartDate();
+        this.expirationDate = product.getExpirationDate();
+    }
+}
